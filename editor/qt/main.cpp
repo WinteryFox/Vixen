bf66db01af3e3bad75cd21dabeef191b105ae560
Gareth Coles <gdude2002@gmail.com>
2023-01-03 12:19:05 +0000

    Add Qt test application

---
+++ editor/qt/main.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include "ui_mainwindow.h"  // CMake will automatically compile mainwindow.ui -> ui_mainwindow.h if it's included.
+
+int main(int argc, char ** argv) {
+    QApplication app {argc, argv};
+
+    QMainWindow mainWindow;
+    Ui::MainWindow mainWindowUi;
+
+    mainWindowUi.setupUi(&mainWindow);
+    mainWindow.show();
+
+    return app.exec(); // NOLINT(readability-static-accessed-through-instance)
+}
