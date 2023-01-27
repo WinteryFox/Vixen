9dd7fc3ad1f5b50fd31ecb3c414c84561ae774cf
Amy <dragonhel5@gmail.com>
2023-01-27 18:17:17 +0100

    fix Vulkan function pointer error

--- engine/vk/VkVixen.cpp
+++ engine/vk/VkVixen.cpp
@@ -1,8 +1,3 @@
-#define VOLK_IMPLEMENTATION
-#define VMA_IMPLEMENTATION
-#define VMA_STATIC_VULKAN_FUNCTIONS 0
-#define VMA_DYNAMIC_VULKAN_FUNCTIONS 0
-
 #include "VkVixen.h"
 
 namespace Vixen::Engine {
